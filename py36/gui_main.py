986816dd7b36934353d6407f2da90efe68ffda84
dh-sung <dh-sung@naver.com>
2021-07-09 10:45:01 +0900

    add new feature

--- py36/gui_main.py
+++ py36/gui_main.py
@@ -7,10 +7,12 @@ import rospy
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--type", type=int, default=0, help="type 0: raw only, type 1: filter only")
+parser.add_argument("--filter_type", type=int, default=0, help="type 0: raw only, type 1: filter only")
+parser.add_argument("--skel_type", type=int, default=0, help="type 0: rigid shoulder, type 1: 3dof shoulder")
 parser.add_argument("--viz_hz", type=int, default=10, help="meshcat viz update hz")
 parser.add_argument("--max_buffer", type=int, default=200, help="pyqtgraph max buffer size")
 parser.add_argument("--plot_hz", type=int, default=100, help="pyqtgraph update hz")
+# parser.add_argument("--skel_hz", type=int, default=100, help="pyqtgraph update hz")
 
 
 args = parser.parse_args()
@@ -18,6 +20,6 @@ args = parser.parse_args()
 if __name__ == "__main__":
     rospy.init_node("motion_filter_gui_node")
     app = QApplication(sys.argv)
-    form = MainGUI(args.type, args.viz_hz, args.max_buffer, args.plot_hz)
+    form = MainGUI(args.filter_type, args.viz_hz, args.max_buffer, args.plot_hz, args.skel_type)
     form.show()
     app.exec_()
--- py36/motion_filters/gui.py
+++ py36/motion_filters/gui.py
@@ -19,10 +19,12 @@ from scipy.spatial.transform import Rotation as R
 from std_msgs.msg import Float64MultiArray
 
 NUM_TRACKER = 6
-
+def quatxyzw2wxyz(xyzw):
+    return np.hstack([xyzw[-1:], xyzw[:3]])
 
 class MeshCatViz:
-    def __init__(self, viz_type):
+    def __init__(self, viz_type, skel_type):
+        self.skel_type = skel_type
         self.viz_type = viz_type
         self.vis = meshcat.Visualizer()
         self.url = self.vis.url()
@@ -31,6 +33,8 @@ class MeshCatViz:
         self.colors = ["0xfc1403", "0x034efc"]  # Red, Blue
         self.bias_y = 1.5  # TODO change to widget
         self.vertices = dict()
+
+        self.quat_hands = None
         # setting subscriber
         self.set_subscribe()
 
@@ -45,11 +49,24 @@ class MeshCatViz:
                 rospy.Subscriber(f"/{self.prefixes[j]}posquat{i}", Float64MultiArray, self.tracker_callback)
 
     def skel_callback(self, msg):
-        data = np.array(msg.data, dtype=np.float64).reshape(-1, 3).T
-        # print(data.shape)
-        self.vertices["spines"] = data[:, :3]
-        self.vertices["larm"] = data[:, 3:8]
-        self.vertices["rarm"] = data[:, 8:13]         
+        data = np.array(msg.data[:-8], dtype=np.float64).reshape(-1, 3).T
+
+        # print(np.array(msg.data).shape)
+        if self.skel_type == 0:
+            # self.vertices["spines"] = data[:, :3]     
+            self.vertices["body"] = np.hstack([data[:,0:1], data[:,3:4], data[:,7:8], data[:, 0:1]])
+            self.vertices["larm"] = data[:, 3:7]
+            self.vertices["rarm"] = data[:, 7:11]
+            self.quat_hands = np.array(msg.data[-8:])
+        elif self.skel_type == 1:
+            # self.vertices["spines"] = data[:, :3]
+            self.vertices["body"] = np.hstack([data[:,0:1], data[:,4:5], data[:,3:4], data[:,9:10], data[:,0:1]])
+            self.vertices["larm"] = data[:, 4:8]
+            self.vertices["rarm"] = data[:, 9:13]
+            self.quat_hands = np.array(msg.data[-8:])
+        else:
+            raise NotImplementedError(f"Unknown skel type{self.skel_type}")
+            
 
     def tracker_callback(self, msg):
         topic_name = msg._connection_header["topic"][1:]
@@ -78,11 +95,21 @@ class MeshCatViz:
         for i in range(NUM_TRACKER):
             self.vis[f"{self.prefixes[viz_type]}posquat{i}"].set_object(
                 g.ObjMeshGeometry.from_file(os.path.join(asset_dir, "../assets/Vive_Tracker_meter.obj")),
-                g.MeshLambertMaterial(color=self.colors[viz_type], reflectivity=0.8),
+                g.MeshLambertMaterial(color=self.colors[viz_type], reflectivity=0.8, opacity=0.4),
             )
             T = np.eye(4)
             T[:3, 3] = np.random.rand(3)
             self.tfs[f"{self.prefixes[viz_type]}posquat{i}"] = T
+
+        self.vis["lhand"].set_object(
+                g.ObjMeshGeometry.from_file(os.path.join(asset_dir, "../assets/lefthand.obj")),
+                g.MeshLambertMaterial(color="0x061099", reflectivity=0.8, opacity=1.0),
+            )
+        self.vis["rhand"].set_object(
+                g.ObjMeshGeometry.from_file(os.path.join(asset_dir, "../assets/righthand.obj")),
+                g.MeshLambertMaterial(color="0x061099", reflectivity=0.8, opacity=1.0),
+            )
+        
 
     def update(self):
         # print("update")
@@ -91,18 +118,35 @@ class MeshCatViz:
 
     def set_transform(self, viz_type):
         size = 0.02
+        linewidth = 20
         for i in range(NUM_TRACKER + 1):
             self.vis[f"{self.prefixes[viz_type]}posquat{i}"].set_transform(self.tfs[f"{self.prefixes[viz_type]}posquat{i}"])
-        if len(self.vertices.keys()) >= 3:
-            self.vis["spines"].set_object(g.Line(g.PointsGeometry(self.vertices["spines"]), g.LineBasicMaterial(color="0x034efc", linewidth=10 )))
-            self.vis["spines_point"].set_object(g.PointsGeometry(self.vertices["spines"]), g.PointsMaterial(size=size))
 
-            self.vis["larm"].set_object(g.Line(g.PointsGeometry(self.vertices["larm"]), g.LineBasicMaterial(color="0x034efc", linewidth=10 )))
+        if len(self.vertices.keys()) >= 3:
+            self.vis["body"].set_object(g.Line(g.PointsGeometry(self.vertices["body"]), g.LineBasicMaterial(color="0x061099", linewidth=linewidth )))
+
+            # self.vis["spines"].set_object(g.Line(g.PointsGeometry(self.vertices["spines"]), g.LineBasicMaterial(color="0x061099", linewidth=linewidth )))
+            # self.vis["spines_point"].set_object(g.PointsGeometry(self.vertices["spines"]), g.PointsMaterial(size=size))
+
+            self.vis["larm"].set_object(g.Line(g.PointsGeometry(self.vertices["larm"]), g.LineBasicMaterial(color="0x061099", linewidth=linewidth )))
             self.vis["larm_point"].set_object(g.PointsGeometry(self.vertices["larm"]), g.PointsMaterial(size=size))
 
-            self.vis["rarm"].set_object(g.Line(g.PointsGeometry(self.vertices["rarm"]), g.LineBasicMaterial(color="0x034efc", linewidth=10)))
+            self.vis["rarm"].set_object(g.Line(g.PointsGeometry(self.vertices["rarm"]), g.LineBasicMaterial(color="0x061099", linewidth=linewidth)))
             self.vis["rarm_point"].set_object(g.PointsGeometry(self.vertices["rarm"]), g.PointsMaterial(size=size))
 
+            T_l = np.eye(4)
+            T_r = np.eye(4)
+
+            T_l[:3, 3] = self.vertices["larm"][:, -2]
+            T_r[:3, 3] = self.vertices["rarm"][:, -2]
+
+            T_l[:3, :3] = tf.quaternion_matrix(quatxyzw2wxyz(self.quat_hands[:4]))[:3,:3]
+            T_r[:3, :3] = tf.quaternion_matrix(quatxyzw2wxyz(self.quat_hands[4:]))[:3,:3]
+            
+            self.vis["lhand"].set_transform(T_l @ tf.euler_matrix(np.pi/2.0, 0.0, -np.pi/2.0) @ tf.translation_matrix(np.array([- 0.1, 0.0, 0.0])))
+            self.vis["rhand"].set_transform(T_r @ tf.euler_matrix(np.pi/2.0, 0.0, np.pi/2.0) @ tf.translation_matrix(np.array([ - 0.1, 0.0, 0.0])))
+
+            # print(T_l, T_r)
 class PlotGUI:
     def __init__(self, max_buffer, plot_hz):
         # do something
@@ -166,9 +210,10 @@ class PlotGUI:
 
 
 class MainGUI(QMainWindow):
-    def __init__(self, meshcat_viz_type, vis_hz, plot_buffer, plot_hz):
+    def __init__(self, meshcat_viz_type, vis_hz, plot_buffer, plot_hz, meshcat_skel_type):
         QMainWindow.__init__(self)
         self.meshcat_viz_type = meshcat_viz_type
+        self.meshcat_skel_type = meshcat_skel_type
         self.vis_hz = vis_hz
         self.plot_buffer = plot_buffer
         self.plot_hz = plot_hz
@@ -181,7 +226,7 @@ class MainGUI(QMainWindow):
         layout = QGridLayout()
 
         # web browser widget
-        self.meshcat_viz = MeshCatViz(self.meshcat_viz_type)
+        self.meshcat_viz = MeshCatViz(self.meshcat_viz_type, self.meshcat_skel_type)
         url = self.meshcat_viz.url
         web = QWebEngineView()  # browser in qtgui
         web.load(QUrl(url))
